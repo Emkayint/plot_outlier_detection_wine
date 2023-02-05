892d2d370c5275885420ef97c3c8c74ea82d2df5
Kariuki <mkmartinoes@gmail.com>
2023-02-05 20:20:23 +0300

    get data from wine function

--- plot_outlier_detection_wine.py
+++ plot_outlier_detection_wine.py
@@ -12,3 +12,11 @@ classifiers = {
   "Empirical Covariance": EllipticEnvelope(support_fraction=1.0, contamination=0.25),  "Robust Covariance (Minimum Covariance Determinant)" : EllipticEnvelope(contamination=0.25), "OCSVM": OneClassSVM(nu=0.25, gamma=0.35)
 }
 
+colors = ["m", "g", "b"]
+legend1 = {}
+legend2 = {}
+
+# get data
+X1 = load_wine()["data"][:, [1, 2]] # two clusters
+
+print(X1)
