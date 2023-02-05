b0689d5fad5d71927f402ef955b0bb22bf0323ed
Kariuki <mkmartinoes@gmail.com>
2023-02-05 20:28:33 +0300

    get data from wine function

--- plot_outlier_detection_wine.py
+++ plot_outlier_detection_wine.py
@@ -16,7 +16,19 @@ colors = ["m", "g", "b"]
 legend1 = {}
 legend2 = {}
 
-# get data
+# get data from wine function dataset
+
 X1 = load_wine()["data"][:, [1, 2]] # two clusters
 
-print(X1)
+# Learn a frontier for outlier detection with several classifiers 
+xx1, yy1 = np.meshgrid(np.linspace(0, 6, 500), np.linspace(1, 4.5, 500))
+
+for i, (clf_name, clf) in enumerate(classifiers.items()):
+  plt.figure(1)
+  clf.fit(X1)
+  Z1 = clf.decison_function(np.c_[xx1.ravel(), yy1.ravel()])
+  Z1 = Z1.reshape(xx1.shape)
+  legend1[clf_name] = plt.contour(xx1, yy1, Z1, levels=[0], linewidths=2, colors=colors[i])
+
+legend1_values_list = list(legend1.values())
+legend1_keys_list = list(legend1.keys())
