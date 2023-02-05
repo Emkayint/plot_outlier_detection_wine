d6947c3322524b2feba261b376b8deb8015215d5
Kariuki <mkmartinoes@gmail.com>
2023-02-05 20:06:33 +0300

    imports

---
+++ plot_outlier_detection_wine.py
@@ -0,0 +1,6 @@
+import numpy as nu
+from sklearn.covariance import ElliptivEnvelope
+from sklearn.svm import OneClassSVM
+import matplotlib.pyplot as plt
+import matplotlib.font_manager
+from sklearn.datasets import load_mine
