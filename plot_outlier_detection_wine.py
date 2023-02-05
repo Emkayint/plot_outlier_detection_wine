ce457d03d246c2144786909b812de269811eed38
Kariuki <mkmartinoes@gmail.com>
2023-02-05 20:17:36 +0300

    classifiers

--- plot_outlier_detection_wine.py
+++ plot_outlier_detection_wine.py
@@ -1,6 +1,14 @@
-import numpy as nu
-from sklearn.covariance import ElliptivEnvelope
+# Author: Martin Kariuki <>
+# License: MIT
+import numpy as np
+from sklearn.covariance import EllipticEnvelope
 from sklearn.svm import OneClassSVM
 import matplotlib.pyplot as plt
 import matplotlib.font_manager
-from sklearn.datasets import load_mine
+from sklearn.datasets import load_wine
+
+# Define classifiers
+classifiers = {
+  "Empirical Covariance": EllipticEnvelope(support_fraction=1.0, contamination=0.25),  "Robust Covariance (Minimum Covariance Determinant)" : EllipticEnvelope(contamination=0.25), "OCSVM": OneClassSVM(nu=0.25, gamma=0.35)
+}
+
